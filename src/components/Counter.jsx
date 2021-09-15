cf33682f3809b015a746ef798802e9c509efe5ef
Felipe <fellipefreiire3@gmail.com>
2021-09-14 22:34:38 -0300

    react state

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={() => setCounter(counter + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
